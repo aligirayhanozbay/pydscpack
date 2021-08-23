0a44edab9eee32aa4c3865166a4257a7db9d6eea
Ali Girayhan Ozbay <giray@snorlax.broadband>
2021-08-23 17:34:17 +0100

    optim flags

--- test_airfoil.sh
+++ test_airfoil.sh
@@ -1,5 +1,5 @@
 rm /tmp/annulus.png /tmp/z.png
-python3 -m numpy.f2py -c Src/Dp/src.f -m dsc
+python3 -m numpy.f2py --opt="-O3" --arch="-march=native" -c Src/Dp/src.f -m dsc
 python3 testdsc_airfoil.py 
 xdg-open /tmp/annulus.png 
 xdg-open /tmp/z.png
