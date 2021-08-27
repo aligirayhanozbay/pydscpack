5fb5493ec49b078267776b1b1480d63393fd2c9b
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2021-08-27 15:08:57 +0000

    omp flags

--- test_triangle.sh
+++ test_triangle.sh
@@ -1,5 +1,5 @@
 rm /tmp/annulus.png /tmp/z.png
-python3 -m numpy.f2py -c Src/Dp/src.f -m dsc
+python3 -m numpy.f2py --f77flags='-fopenmp' --f90flags='-fopenmp' --opt='-mcpu=native' --opt='-O3' -c Src/Dp/src.f -m dsc -lgomp
 python3 testdsc.py 
 xdg-open /tmp/annulus.png 
 xdg-open /tmp/z.png
