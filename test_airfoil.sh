b1a4989d24f507a159cb9aa9ef8db5f1822cd9fb
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2021-11-10 17:07:19 +0000

    removed spurious development files

--- test_airfoil.sh
+++
@@ -1,7 +0,0 @@
-rm /tmp/annulus.png /tmp/z.png
-python3 -m numpy.f2py --opt="-O3" --arch="-march=native" --opt="-fopenmp" -c Src/Dp/src.f -m dsc -lgomp
-python3 testdsc_airfoil.py 
-python3 -m numpy.f2py --f77exec=flang --f90exec=flang --opt="-O3" --arch="-march=native" --opt="-fopenmp=libgomp" -c Src/Dp/src.f -m dsc -lgomp
-python3 testdsc_airfoil.py 
-xdg-open /tmp/annulus.png 
-xdg-open /tmp/z.png
